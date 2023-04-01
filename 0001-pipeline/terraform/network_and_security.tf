bc70d4de7f78187a96c0865c8368094af6f840e5
Fernando Goncalves <fernandohmgoncalves@gmail.com>
2023-03-31 17:37:54 -0700

    feat: Adding new EC2 instances and its respective configuration
    In this commit, various EC2 instances were added in accordance with the pipeline project
    The respective instances are being configured with their necessary ports and access from
    particular security groups only
    The bastion host was also added and it is the only way to access internal servers in the same subnet
    since there is no public IP assigned to them

--- 0001-pipeline/terraform/network_and_security.tf
+++ 0001-pipeline/terraform/network_and_security.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "vpc-0" {
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name                 = "vpc_0"
+    Name                 = "vpc-0"
     Created_by_Terraform = "True"
     Environment          = "DEV"
   }
@@ -15,39 +15,135 @@ resource "aws_vpc" "vpc-0" {
 resource "aws_subnet" "subnet-0" {
   cidr_block        = cidrsubnet(aws_vpc.vpc-0.cidr_block, 3, 1)
   vpc_id            = aws_vpc.vpc-0.id
-  availability_zone = "us-west-1a"
+  availability_zone = var.az
   tags = {
-    Name                 = "subnet_0"
+    Name                 = "subnet-0"
     Created_by_Terraform = "True"
     Environment          = "DEV"
   }
 }
 
+# resource "aws_subnet" "subnet-elk" {
+#   cidr_block = cidrsubnet(aws_vpc.vpc-0.cidr_block, 3, 1)
+#   vpc_id = aws_vpc.vpc-0.id
+#   availability_zone = var.az
+#   tags = {
+#     Name                 = "subnet_elk"
+#     Created_by_Terraform = "True"
+#     Environment          = "DEV"
+#   }
+# }
+
 // SECURITY_GROUPS
 resource "aws_security_group" "security-group-0" {
-  name   = "allow-all-sg"
+  name   = "allow-all-ssh_access"
   vpc_id = aws_vpc.vpc-0.id
 
   ingress {
-    description = "Allow SSH access from the specified cidr block"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH access from the specified cidr block"
   }
   // Terraform removes the default rule
   egress {
-    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1" // "-1" it is to allow all protocols. This is commonly used in the egress rules of a security group to allow all outbound traffic.
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
   }
   tags = {
-    Name                 = "security_group_0"
+    Name                 = "security-group-0"
     Created_by_Terraform = "True"
     Environment          = "DEV"
   }
+}
+// Elasticsearch security group
+resource "aws_security_group" "elastic-sg" {
+  name = "elastic-sg"
+  vpc_id = aws_vpc.vpc-0.id
+
+ingress {
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  security_groups = ["${aws_security_group.security-group-0.id}"]
+  description = "Allow SSH access only from the security-group-0 where the bastion host is"
+  }
+
+ingress {
+  from_port = 8
+  to_port = 0
+  protocol = "icmp"
+  security_groups = ["${aws_security_group.security-group-0.id}"]
+  description = "Allow ping from the security-group-0"
+}
+
+ingress {
+  from_port = 9200
+  to_port = 9200
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "Elasticsearch default port"
+}
+
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1" // "-1" it is to allow all protocols. This is commonly used in the egress rules of a security group to allow all outbound traffic.
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "Allow all outbound traffic"
+  }
+tags = {
+  Name                 = "elastic-sg"
+  Created_by_Terraform = "True"
+  Environment          = "DEV"
+  }
+}
+
+resource "aws_security_group" "kibana-sg" {
+  name = "kibana-sg"
+  vpc_id = aws_vpc.vpc-0.id
+
+ingress {
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  security_groups = ["${aws_security_group.security-group-0.id}"]
+  description = "Allow SSH access only from the security-group-0 where the bastion host is"
+}
+
+ingress {
+  from_port = 8
+  to_port = 0
+  protocol = "icmp"
+  security_groups = ["${aws_security_group.security-group-0.id}"]
+  description = "Allow ping from the security-group-0"
+}
+
+ingress {
+  from_port = 5601
+  to_port = 5601
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "Kibana default port"
+}
+
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1" // "-1" it is to allow all protocols. This is commonly used in the egress rules of a security group to allow all outbound traffic.
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "Allow all outbound traffic"
+  }
+tags = {
+  Name                 = "kibana-sg"
+  Created_by_Terraform = "True"
+  Environment          = "DEV"
+  }
+
 }
 
 // ELASTIC_IP
--- 0001-pipeline/terraform/variables.tf
+++ 0001-pipeline/terraform/variables.tf
@@ -1,22 +1,44 @@
 # variable "ami_name" {}
-variable "us-west-2-ami_id" {
-  type    = string
-  default = "ami-06bb3ee01d992f30d"
+
+variable "ami_id" {
+  type = string
+  default = "ami-00756a2b7a21e2bd3"
+  # us-west-1 ami-id
 }
 
-variable "free_tier_instance_type" {
+# variable "us_west_2_ami_id" {
+#   type    = string
+#   default = "ami-0223246728818f162"
+# }
+
+variable "free_tier" {
   type    = string
   default = "t2.micro"
 }
 
-variable "us_west_1_key" {
+variable "access_key" {
   type    = string
   default = "fhmglearningaws2-us-west-1"
+  # us-west-1 key
 }
 
-variable "us_west_2_key" {
-  type    = string
-  default = "fhmglearningaws2"
+variable "az" {
+  type = string
+  default = "us-west-1a"
 }
+
+# variable "elastic_tcp_ports" {
+#   type = list
+#   default = [9200, 80]
+# }
+
+# variable "kibana_tcp_ports" {
+#   default = [5061]
+# }
+
+# variable "us_west_2_key" {
+#   type    = string
+#   default = "fhmglearningaws2"
+# }
 # variable "ami_key_pair" {}
 # variable "availability_zone" {}
