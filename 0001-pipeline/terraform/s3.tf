0fdab57677590be21591cbd1b599e435ce9135f0
Fernando Goncalves <fernandohmgoncalves@gmail.com>
2023-04-11 17:15:33 -0700

    enhancement: Define S3 as backend for terraform state
    The S3 bucket was created with versionning enabled and the terraform.state migrated to S3 bucket.

---
+++ 0001-pipeline/terraform/s3.tf
@@ -0,0 +1,23 @@
+terraform {
+    backend "s3" {
+      bucket = "0001-project-tf-bucket"
+      key = "terraform/tfstate"
+      region = "us-west-1"
+    }
+}
+
+resource "aws_s3_bucket" "terraform" {
+  bucket = "0001-project-tf-bucket"
+}
+
+resource "aws_s3_bucket_acl" "terraform-acl" {
+  bucket = aws_s3_bucket.terraform.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "terraform-versioning" {
+  bucket = aws_s3_bucket.terraform.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
