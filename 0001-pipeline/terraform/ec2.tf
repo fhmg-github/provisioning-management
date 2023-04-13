f6c50b65403617ddb4e8f343b0bfb2970fe0d997
Fernando Goncalves <fernandohmgoncalves@gmail.com>
2023-04-12 19:55:20 -0700

    refactor: adding S3 bucket config to the repo

--- 0001-pipeline/terraform/ec2.tf
+++ 0001-pipeline/terraform/ec2.tf
@@ -21,50 +21,45 @@ resource "aws_instance" "bastion_host" {
   })
 }
 
-# resource "aws_instance" "maven" {
-#   ami                         = var.ami_id
-#   instance_type               = var.free_tier
-#   key_name                    = var.access_key
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
-#   subnet_id                   = aws_subnet.subnet-0.id
-
-#   tags = merge(
-#     var.env_tags.default_tags, local.timestamp_tags,
-#     {
-#       Name = "maven"
-#   })
-# }
-
-# resource "aws_instance" "Jenkins_master" {
-#   ami                         = var.ami_id
-#   instance_type               = var.free_tier
-#   key_name                    = var.access_key
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
-#   subnet_id                   = aws_subnet.subnet-0.id
-
-#   tags = merge(
-#     var.env_tags.default_tags, local.timestamp_tags,
-#     {
-#       Name = "jenkins_master"
-#   })
-# }
-
-# resource "aws_instance" "JMeter" {
-#   ami                         = var.ami_id
-#   instance_type               = var.free_tier
-#   key_name                    = var.access_key
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
-#   subnet_id                   = aws_subnet.subnet-0.id
-
-#   tags = merge(
-#     var.env_tags.default_tags, local.timestamp_tags,
-#     {
-#       Name = "jmeter"
-#   })
-# }
+ resource "aws_instance" "maven" {
+   ami                         = var.ami_id
+   instance_type               = var.free_tier
+   key_name                    = var.access_key
+   associate_public_ip_address = true
+   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
+   subnet_id                   = aws_subnet.subnet-0.id
+   tags = merge(
+     var.env_tags.default_tags, local.timestamp_tags,
+     {
+       Name = "maven"
+   })
+ }
+ resource "aws_instance" "Jenkins_master" {
+   ami                         = var.ami_id
+   instance_type               = var.free_tier
+   key_name                    = var.access_key
+   associate_public_ip_address = true
+   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
+   subnet_id                   = aws_subnet.subnet-0.id
+   tags = merge(
+     var.env_tags.default_tags, local.timestamp_tags,
+     {
+       Name = "jenkins_master"
+   })
+ }
+ resource "aws_instance" "JMeter" {
+   ami                         = var.ami_id
+   instance_type               = var.free_tier
+   key_name                    = var.access_key
+   associate_public_ip_address = true
+   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
+   subnet_id                   = aws_subnet.subnet-0.id
+   tags = merge(
+     var.env_tags.default_tags, local.timestamp_tags,
+     {
+       Name = "jmeter"
+   })
+ }
 
 resource "aws_instance" "elastic_master" {
   ami                         = var.ami_id
@@ -96,32 +91,29 @@ resource "aws_instance" "kibana" {
   })
 }
 
-# resource "aws_instance" "logstash" {
-#   ami                         = var.ami_id
-#   instance_type               = var.free_tier
-#   key_name                    = var.access_key
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
-#   subnet_id                   = aws_subnet.subnet-0.id
-
-#   tags = merge(
-#     var.env_tags.default_tags, local.timestamp_tags,
-#     {
-#       Name = "logstash"
-#   })
-# }
-
-# resource "aws_instance" "jfrog_oss" {
-#   ami                         = var.ami_id
-#   instance_type               = var.free_tier
-#   key_name                    = var.access_key
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
-#   subnet_id                   = aws_subnet.subnet-0.id
-
-#   tags = merge(
-#     var.env_tags.default_tags, local.timestamp_tags,
-#     {
-#       Name = "jfrog"
-#   })
-# }
+ resource "aws_instance" "logstash" {
+   ami                         = var.ami_id
+   instance_type               = var.free_tier
+   key_name                    = var.access_key
+   associate_public_ip_address = true
+   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
+   subnet_id                   = aws_subnet.subnet-0.id
+   tags = merge(
+     var.env_tags.default_tags, local.timestamp_tags,
+     {
+       Name = "logstash"
+   })
+ }
+ resource "aws_instance" "jfrog_oss" {
+   ami                         = var.ami_id
+   instance_type               = var.free_tier
+   key_name                    = var.access_key
+   associate_public_ip_address = true
+   vpc_security_group_ids      = [aws_security_group.security-group-0.id]
+   subnet_id                   = aws_subnet.subnet-0.id
+   tags = merge(
+     var.env_tags.default_tags, local.timestamp_tags,
+     {
+       Name = "jfrog"
+   })
+ }
--- 0001-pipeline/terraform/s3.tf
+++ 0001-pipeline/terraform/s3.tf
@@ -1,13 +1,14 @@
-terraform {
-    backend "s3" {
-      bucket = "0001-project-tf-bucket"
-      key = "terraform/tfstate"
-      region = "us-west-1"
-    }
-}
+/* terraform {
+  backend "s3" {
+    bucket = "0001-project-tf-bucket"
+    key    = "terraform/tfstate"
+    region = "us-west-1"
+  }
+} */
 
 resource "aws_s3_bucket" "terraform" {
   bucket = "0001-project-tf-bucket"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_acl" "terraform-acl" {
@@ -20,4 +21,7 @@ resource "aws_s3_bucket_versioning" "terraform-versioning" {
   versioning_configuration {
     status = "Enabled"
   }
+  lifecycle {
+    prevent_destroy = false
+}
 }
