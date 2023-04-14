0e29f9bdde20ffd03935b072f8c307a22f6c4d9f
Fernando Goncalves <fernandohmgoncalves@gmail.com>
2023-04-13 19:06:54 -0700

    refactor: Adding security configuration to the EC2 instances
    Added all the security groups for the respective EC2 instances
    There are 2 subnets, public and private. The public will only have the bastion host
    in it and the private will have the remaining instances.
    Only the public subnet has the routing table associated to it and thus the internet gateway associated to that routing table.

--- 0001-pipeline/terraform/network.tf
+++ 0001-pipeline/terraform/network.tf
@@ -13,7 +13,19 @@ resource "aws_vpc" "vpc-0" {
 }
 
 # SUBNET
-resource "aws_subnet" "subnet-0" {
+resource "aws_subnet" "subnet-public" {
+  cidr_block        = cidrsubnet(aws_vpc.vpc-0.cidr_block, 3, 0)
+  vpc_id            = aws_vpc.vpc-0.id
+  availability_zone = var.az
+  tags = merge(
+    var.env_tags.default_tags,
+    {
+      Name        = "subnet-0"
+      CreatedDate = timestamp()
+  })
+}
+
+resource "aws_subnet" "subnet-private" {
   cidr_block        = cidrsubnet(aws_vpc.vpc-0.cidr_block, 3, 1)
   vpc_id            = aws_vpc.vpc-0.id
   availability_zone = var.az
@@ -58,6 +70,6 @@ resource "aws_route_table" "route-table-0" {
 # ROUTING TABLE ASSOCIATION
 
 resource "aws_route_table_association" "subnet-association-0" {
-  subnet_id      = aws_subnet.subnet-0.id
+  subnet_id      = aws_subnet.subnet-public.id
   route_table_id = aws_route_table.route-table-0.id
 }
--- 0001-pipeline/terraform/security.tf
+++ 0001-pipeline/terraform/security.tf
@@ -1,4 +1,4 @@
-# SECURITY_GROUPS
+# PUBLIC SECURITY_GROUPS
 resource "aws_security_group" "security-group-0" {
   name   = "allow-all-ssh_access"
   vpc_id = aws_vpc.vpc-0.id
@@ -27,6 +27,7 @@ resource "aws_security_group" "security-group-0" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow all outbound traffic"
   }
+
   tags = merge(
     var.env_tags.default_tags,
     {
@@ -34,6 +35,127 @@ resource "aws_security_group" "security-group-0" {
       CreatedDate = timestamp()
   })
 }
+
+# MAVEN SECURITY GROUP
+resource "aws_security_group" "maven-sg" {
+  name   = "maven-sg"
+  vpc_id = aws_vpc.vpc-0.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.security-group-0.id]
+    description     = "Allow SSH access only from the security-group-0"
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc-0.cidr_block]
+    #security_groups = [aws_security_group.security-group-0.id, aws_security_group.jenkins-sg.id]
+    description = "Allow ping only from the security-group-0"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = merge(
+    var.env_tags.default_tags,
+    {
+      Name        = "maven-sg"
+      CreatedDate = timestamp()
+  })
+}
+
+# JENKINS SECURITY GROUP
+resource "aws_security_group" "jenkins-sg" {
+  name   = "jenkins-sg"
+  vpc_id = aws_vpc.vpc-0.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.security-group-0.id]
+    description     = "Allow SSH access only from the security-group-0"
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc-0.cidr_block]
+    description = "Allow ping only from the security-group-0"
+  }
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.security-group-0.id]
+    description     = "Allow Jenkins access only from the security-group-0"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = merge(
+    var.env_tags.default_tags,
+    {
+      Name        = "jenkins-sg"
+      CreatedDate = timestamp()
+  })
+}
+
+# MAVEN SECURITY GROUP
+resource "aws_security_group" "jmeter-sg" {
+  name   = "jmeter-sg"
+  vpc_id = aws_vpc.vpc-0.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.security-group-0.id]
+    description     = "Allow SSH access only from the security-group-0"
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc-0.cidr_block]
+    description = "Allow ping only from the security-group-0"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = merge(
+    var.env_tags.default_tags,
+    {
+      Name        = "jmeter-sg"
+      CreatedDate = timestamp()
+  })
+}
+
 # ELASTICSEARCH SG
 
 resource "aws_security_group" "elastic-sg" {
@@ -49,18 +171,19 @@ resource "aws_security_group" "elastic-sg" {
   }
 
   ingress {
-    from_port       = -1
-    to_port         = -1
-    protocol        = "icmp"
-    security_groups = [aws_security_group.security-group-0.id]
-    description     = "Allow ping only from the security-group-0"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc-0.cidr_block]
+    description = "Allow ping only from the security-group-0"
   }
+
   ingress {
     from_port       = 9200
     to_port         = 9200
     protocol        = "tcp"
     security_groups = [aws_security_group.security-group-0.id]
-    description     = "Allow SSH access only from the security-group-0"
+    description     = "Allow ES access only from the security-group-0"
   }
 
   egress {
@@ -92,19 +215,21 @@ resource "aws_security_group" "kibana-sg" {
   }
 
   ingress {
-    from_port       = -1
-    to_port         = -1
-    protocol        = "icmp"
-    security_groups = [aws_security_group.security-group-0.id]
-    description     = "Allow ping only from the security-group-0"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc-0.cidr_block]
+    description = "Allow ping only from the security-group-0"
   }
+
   ingress {
     from_port       = 5106
     to_port         = 5106
     protocol        = "tcp"
     security_groups = [aws_security_group.security-group-0.id]
-    description     = "Allow SSH only from the security-group-0"
+    description     = "Allow access to Kibana only from the security-group-0"
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -112,11 +237,102 @@ resource "aws_security_group" "kibana-sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow all outbound traffic"
   }
+
   tags = merge(
     var.env_tags.default_tags,
     {
       Name        = "kibana-sg"
       CreatedDate = timestamp()
   })
-
 }
+
+# LOGSTASH SG
+resource "aws_security_group" "logstash-sg" {
+  name   = "logstash-sg"
+  vpc_id = aws_vpc.vpc-0.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.security-group-0.id]
+    description     = "Allow SSH only from the security-group-0"
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc-0.cidr_block]
+    description = "Allow ping only from the security-group-0"
+  }
+
+  ingress {
+    from_port       = 9600
+    to_port         = 9600
+    protocol        = "tcp"
+    security_groups = [aws_security_group.security-group-0.id]
+    description     = "Allow access to Kibana only from the security-group-0"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = merge(
+    var.env_tags.default_tags,
+    {
+      Name        = "logstash-sg"
+      CreatedDate = timestamp()
+  })
+}
+
+# JFROG SG
+resource "aws_security_group" "jfrog-sg" {
+  name   = "jfrog-sg"
+  vpc_id = aws_vpc.vpc-0.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.security-group-0.id]
+    description     = "Allow SSH only from the security-group-0"
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc-0.cidr_block]
+    description = "Allow ping only from the security-group-0"
+  }
+
+  ingress {
+    from_port       = 9600
+    to_port         = 9600
+    protocol        = "tcp"
+    security_groups = [aws_security_group.security-group-0.id]
+    description     = "Allow access to Kibana only from the security-group-0"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = merge(
+    var.env_tags.default_tags,
+    {
+      Name        = "logstash-sg"
+      CreatedDate = timestamp()
+  })
+}
+
