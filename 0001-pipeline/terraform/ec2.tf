ebda813ec913f177c945fe767f0aa4853395047e
Fernando Goncalves <87397476+fhmg-github@users.noreply.github.com>
2023-10-01 17:22:53 -0700

    fix:Keeping Jenkins to have an public IP associated (#22)

--- 0001-pipeline/terraform/ec2.tf
+++ 0001-pipeline/terraform/ec2.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "jenkins_master" {
   instance_type = var.free_tier
   key_name      = var.private_instances_access_key
 
-  associate_public_ip_address = false
+  associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
   subnet_id                   = aws_subnet.subnet-private.id
   private_ip                  = "10.0.34.210"
