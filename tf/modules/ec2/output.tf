8a58ce52e7eeae33c1bacd524f6503dc2b7c75ab
Fernando Goncalves <fernandohmgoncalves@gmail.com>
2023-10-28 13:41:09 -0700

    commit: just adding the ec2 output file from modules folder

--- tf/modules/ec2/output.tf
+++ tf/modules/ec2/output.tf
@@ -2,3 +2,13 @@ output "instance_ids" {
   value       = aws_instance.ec2.id
   description = "IDs of EC2 instances"
 }
+
+output "instance_pub_ip" {
+  value = aws_instance.ec2.public_ip
+  description = "Instance's public IP address"
+}
+
+output "instance_priv_ip" {
+  value = aws_instance.ec2.private_ip
+  description = "Instance's private IP address"
+}
